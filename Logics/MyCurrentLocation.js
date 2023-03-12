25b6fd8addfb815c931848d08d694cb58f6a229f
sonukryadav <sonukryadav56@gmail.com>
2023-03-12 13:06:20 +0530

    location

--- Logics/MyCurrentLocation.js
+++ Logics/MyCurrentLocation.js
@@ -1,1 +1,14 @@
 import * as Location from 'expo-location';
+
+const MyCurrentLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+        setErrorMsg('Permission to access location was denied');
+        return;
+    }
+    let location = await Location.requestForegroundPermissionsAsync();
+    console.log(location);
+}
+
+
+export default MyCurrentLocation;
